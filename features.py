eb3b4556203badc98a18ac85efb913c4613920b8
Ayoub ELHARRAN <elharran.ayoub@inemail.ine.inpt.ma>
2023-10-28 18:53:31 +0100

    new features

--- features.py
+++ features.py
@@ -3,3 +3,19 @@ def no_paths(L):
 
 def no_points(L):
     return sum([len(l) for l in L])
+
+def width(L):
+    w=0
+    for i in range(no_paths(L)):
+        width = max([l[0] for l in L[i]]) - min([l[0] for l in L[i]])
+        if width > w:
+            w = width
+    return w
+
+def height(L):
+    h=0
+    for i in range(no_paths(L)):
+        height = max([l[1] for l in L[i]]) - min([l[1] for l in L[i]])
+        if height > h:
+            h = height
+    return h
