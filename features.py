c55d7e001642a0b4c08d77a25c6745e327a60235
Ayoub ELHARRAN <elharran.ayoub@inemail.ine.inpt.ma>
2023-10-29 23:55:41 +0100

    features

--- features.py
+++ features.py
@@ -20,6 +20,10 @@ def height(draw):
             h = height
     return h
 
+def elongation(draw):
+    w, h = width(draw), height(draw)
+    return (1 + max(w,h))/(1 + min(w,h))
+
 def lenght(draw):
     l=0
     for i in range(no_paths(draw)):
@@ -30,3 +34,11 @@ def distance(A,B):
     xa,ya = A
     xb,yb = B
     return sqrt((xb - xa)**2 + (yb - ya)**2)
+
+def triangle_area(A,B,C):
+    a = distance(A,B)
+    b = distance(B,C)
+    c = distance(C,A)
+    p = (a+b+c)/2
+    return (p*(p-a)(p-b)(p-c))**.5
+
