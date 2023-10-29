a024b60c27d3eba9f66abbcfd6fea5574fd75850
Ayoub ELHARRAN <elharran.ayoub@inemail.ine.inpt.ma>
2023-10-29 17:54:06 +0100

    features

--- features.py
+++ features.py
@@ -1,21 +1,32 @@
-def no_paths(L):
-    return len(L)
+def no_paths(draw):
+    return len(draw)
 
-def no_points(L):
-    return sum([len(l) for l in L])
+def no_points(draw):
+    return sum([len(l) for l in draw])
 
-def width(L):
+def width(draw):
     w=0
-    for i in range(no_paths(L)):
-        width = max([l[0] for l in L[i]]) - min([l[0] for l in L[i]])
+    for i in range(no_paths(draw)):
+        width = max([l[0] for l in draw[i]]) - min([l[0] for l in draw[i]])
         if width > w:
             w = width
     return w
 
-def height(L):
+def height(draw):
     h=0
-    for i in range(no_paths(L)):
-        height = max([l[1] for l in L[i]]) - min([l[1] for l in L[i]])
+    for i in range(no_paths(draw)):
+        height = max([l[1] for l in draw[i]]) - min([l[1] for l in draw[i]])
         if height > h:
             h = height
     return h
+
+def lenght(draw):
+    l=0
+    for i in range(no_paths(draw)):
+        ...
+
+def distance(A,B):
+    from math import sqrt
+    xa,ya = A
+    xb,yb = B
+    return sqrt((xb - xa)**2 + (yb - ya)**2)
